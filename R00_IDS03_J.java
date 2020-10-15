85bae205010631fa4a81a231b43d229c60fabb5f
Chris Melton <chris.melton314@knights.ucf.edu>
2020-10-15 16:31:19 -0400

    Added mandatory rule 1

---
+++ R00_IDS03_J.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+public class R00_IDS03_J
+{
+	public static void main(String[] args)
+	{
+		boolean loginSuccessful = true;
+		Logger logger = Logger.getLogger(R00_IDS03_J.class.getName());
+		String username = "chris";
+
+		// Rule 00. Input Validation and Data Sanitization (IDS)
+		// IDS03-J. Do not log unsanitized user input.
+		// Below is a non-compliant segment of code:
+		if(loginSuccessful)
+		{
+			logger.severe("User login succeeded for: " + username);
+		}
+		else
+		{
+			logger.severe("User login failed for: " + username);
+		}
+
+		// Correct the code as shown in the Compliant Solution below:
+		if(loginSuccessful)
+		{
+			logger.severe("User login succeeded for: " + sanitizeUser(username));
+		}
+		else
+		{
+			logger.severe("User login failed for: " + sanitizeUser(username));
+		}
+	}
+
+	public static String sanitizeUser(String username)
+	{
+		return Pattern.matches("[A-Za-z0-9_]+", username) ? username : "unauthorized user";
+	}
+}
